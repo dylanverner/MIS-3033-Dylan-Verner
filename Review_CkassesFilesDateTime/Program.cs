1191a8b358fd90a45be374d593ab6fa5b37e066b
Dylan Verner <Dylan.N.Verner-1@ou.edu>
2020-08-31 12:06:34 -0500

    rectangle class

--- Review_CkassesFilesDateTime/Program.cs
+++ Review_CkassesFilesDateTime/Program.cs
@@ -6,7 +6,23 @@ namespace Review_CkassesFilesDateTime
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Rectangle rect1 = new Rectangle();
+            rect1.height = 10;
+            rect1.width = 20;
+
+            Console.WriteLine($"the are of rect1 is {rect1.area()}");
+
+
+            List<Rectangle> rectangles = new List<Rectangle>();
+            rectangles.add(rect1);
+            rectangles.add(rect2);
+
+            rectangles.Add(new Rectangle(7, 7));
+
+            foreach (var rect in Rectangle)
+            {
+                Console.WriteLine()
+            }
         }
     }
 }
---
+++ Review_CkassesFilesDateTime/Rectangle.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Review_CkassesFilesDateTime
+{
+    class Rectangle
+    {
+        public int width { get;  set; }
+
+        public int height {get; set;}
+
+        //default empty constructor
+
+        public Rectangle()
+        {
+
+        }
+
+        //oborloaded constructor for the Rectangle Class
+
+        public Rectangle(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+
+             }
+
+        //calculate the area of the Rectangle
+            public double area()
+            {
+
+            area = length * width;
+
+            return double;
+
+
+            }
+
+
+        
+
+        
+
+    }
+}
